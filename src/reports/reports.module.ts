cdc4dfec606c050265b5dfb6e46130dba748812c
Mateus Silva <mateus_silva97@hotmail.com>
2023-06-14 19:36:39 -0300

    feat: add new queue
    after you need up the container and run the followings command
    
    docker compose exec bash app
    
    npx nest g service reports/reports-job
    
    it create  the new service

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports/reports.service';
 import { ReportsController } from './reports/reports.controller';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'reports',
+    }),
+  ],
   providers: [ReportsService],
-  controllers: [ReportsController]
+  controllers: [ReportsController],
 })
 export class ReportsModule {}
